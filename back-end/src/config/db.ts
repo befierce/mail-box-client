4822f22ba296aae0102f6ee2dae09e7f3436db9d
manoj bhatt <enterbhatt123@gmail.com>
2025-08-02 16:37:33 +0530

    feat: connected to mongo Atlas ditched mongo compass

--- back-end/src/config/db.ts
+++ back-end/src/config/db.ts
@@ -1,13 +1,16 @@
 import mongoose from "mongoose";
+const MONGO_URL = process.env.MONGO_URL as string;
 
+console.log("mongo url", MONGO_URL);
 const connectDB = async () => {
-  try{
-    const conection = await mongoose.connect("mongodb://localhost:27017/mail-box-client");
+  try {
+    const conection = await mongoose.connect(
+      MONGO_URL
+    );
     console.log("connected to mongoDB");
-  }catch(error){
-    console.error("mongodb connection error",error)
+  } catch (error) {
+    console.error("mongodb connection error", error);
   }
 };
-
 
 export default connectDB;
